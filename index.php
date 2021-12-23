87643a7f01ed4a8c3e6e53b0308d7978a67bbc9c
dinin538 <dnnblh@gmail.com>
2021-12-23 10:32:34 +0800

    up

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <title>Dobi Giant Bubbles</title>
 </head>
 <body>
-    <marquee direction="right" scrollamount="20">
+    <marquee direction="right" scrollamount="17">
     <img src="dobi.jpg" alt="Trulli" width="200" height="200">
     <img src="d1.jpg" alt="Trulli" width="300" height="200">
     <img src="d2.jpg" alt="Trulli" width="300" height="200">
@@ -19,6 +19,42 @@
     <h1>Dobi Giant Bubbles</h1>
     <p>15G, Jalan Makyong 5B/KU5, Bandar Bukit Raja, Klang, Selangor 41050 Klang, Selangor</p>
 
+    <h2>Service that AVAILABLE in our service</h2> 
+ 
+<table style="width:100%"> 
+  <tr> 
+    <th> Normal Laundry </th> 
+    <th> Dry Cleaning </th> 
+    <th> Hand Cleaning </th> 
+    <th> Special Wash and Dry (Washable Carpet) </th> 
+  </tr> 
+  <tr> 
+    <td>Normal Clothes </td> 
+    <td>Please contact our company</td> 
+    <td>Normal Clothes</td> 
+    <td> RM 15.00 - RM 30.00 ( per piece )</td> 
+  </tr> 
+  <tr> 
+    <td>RM 2.00 ( per kg )</td> 
+    <td> Will personal inform the customer </td> 
+    <td>RM 5.00 ( per kg )</td> 
+    <th> Special Service (Clothes Iron) </th> 
+  </tr> 
+  <tr> 
+    <td>Thick Clothes ( Jeans or Towel) </td> 
+    <th> Special Wash and Dry (Matress , Curtain , Blanket) </th> 
+    <th> Special Wash and Dry (Comforter, Toto) </th> 
+    <td>RM 1.00 - RM 2.00 ( per piece )</td> 
+  </tr> 
+  <tr> 
+    <td>RM 0.50 ( per piece )</td> 
+    <td> RM 4.00 per kg  </td> 
+    <td> RM 12.00 - RM 18.00 ( per piece )  </td> 
+    <th> Special Service (Pick up or Delivery) </th> 
+  </tr> 
+</table> 
+
+<button type="submit" name="Send" class="registerbtn">Submit</button>
     
 </body>
 </html>
