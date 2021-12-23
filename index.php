4042d179840932da7219290582bfac39df564c85
dinin538 <dnnblh@gmail.com>
2021-12-23 09:19:26 +0800

    up

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Dobi Giant Bubbles</title>
+</head>
+<body>
+    <img src="dobi.jpg" alt="Trulli" width="200" height="200" class="center">
+    <h1>Dobi Giant Bubbles</h1>
+    <p>15G, Jalan Makyong 5B/KU5, Bandar Bukit Raja, Klang, Selangor 41050 Klang, Selangor</p>
+</body>
+</html>
