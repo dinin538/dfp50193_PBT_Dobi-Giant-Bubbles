591c7e6df1d447c19cc9f712b06a667bb9366dc8
dinin538 <dnnblh@gmail.com>
2021-12-30 08:29:32 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,57 +8,61 @@
     <link rel="stylesheet" href="style.css">
     <title>Dobi Giant Bubbles</title>
 </head>
-<body>
-    
-</div>
-    <h1>Dobi Giant Bubbles</h1>
-    <p>15G, Jalan Makyong 5B/KU5, Bandar Bukit Raja, Klang, Selangor 41050 Klang, Selangor</p>
 
-    <h2>Service that AVAILABLE in our service</h2> 
+<body>
+
+    </div>
+    <h1>Dobi Giant Bubbles</h1>
+    <p align="center">15G, Jalan Makyong 5B/KU5, Bandar Bukit Raja, Klang, Selangor 41050 Klang, Selangor</p>
+
+    <h2>Service that AVAILABLE in our service</h2>
 
     <form name="myform" method="post" action="result.php">
-    <div class="container">
-      
-        <p>
-            <label for="nl"><b>Normal Laundry</b></label>
-        </p>
-        <p>
-            <input type="radio" name="dc" id="dc" required>
-            <label for="dc"><b>Normal Clothes  RM 15.00 - RM 30.00 ( per piece )</b></label>
-        </p><br>
+        <div class="container">
 
-        <p>
-            <input type="radio" name="cc" id="cc" required>
-            <label for="cc"><b>Dry Cleaning RM 2.00 ( per kg )</b></label>
+            <p>
+                <label for="nl"><b>Normal Laundry</b></label>
+            </p>
+            <p>
+                <input type="radio" name="dc" id="dc" required>
+                <label for="dc"><b>Normal Clothes RM 15.00 - RM 30.00 ( per piece )</b></label>
+            </p>
+            <P>
+                <input type="checkbox" id="steam" name="spesial" value="steam">
+                <label for="spesial"> Special Steam Iron</label>
 
-            
-        </p>
-        
-        <p>
-            <input type="checkbox" id="spesial" name="spesial" value="iron">
-            <label for="spesial"> Special Service (Clothes Iron) RM 5.00 ( per kg )</label><br>
-          </p><br>
+            </P>
+            <p>
+                <input type="radio" name="cc" id="cc" required>
+                <label for="cc"><b>Dry Cleaning RM 2.00 ( per kg )</b></label>
+            </p>
 
-        <p>
-            <label for="cur"><b>Hand Cleaning</b></label>
-        </p>
-        
-        <p>
+            <p>
+                <input type="checkbox" id="spesial" name="spesial" value="iron">
+                <label for="spesial"> Special Service (Clothes Iron) RM 5.00 ( per kg )</label><br>
+            </p><br>
 
-          <input type="radio" id="jeans" name="jeans" value="jeans">
-          <label for="jeans"> Thick Clothes ( Jeans or Towel)  <b>RM 1.00 - RM 2.00 ( per piece )</b></label>
-          <input type="checkbox" id="spesial" name="spesial" value="iron">
-          <label for="spesial"> Special Wash and Dry (Matress , Curtain , Blanket)</label> 
+            <p>
+                <label for="cur"><b>Hand Cleaning</b></label>
+            </p>
+            <p>
 
-        </p>
+                <input type="radio" id="jeans" name="jeans" value="jeans">
+                <label for="jeans"> Thick Clothes ( Jeans or Towel) <b>RM 1.00 - RM 2.00 ( per piece )</b></label>
+            </p>
+            <p>
+                <input type="checkbox" id="spesial" name="spesial" value="iron">
+                <label for="spesial"> Special Wash and Dry (Matress , Curtain , Blanket)</label>
+            </p>
 
-        <p>
+            <p>
 
-        </p>
-      
-        
-        <button type="submit" name="Send" class="registerbtn">Submit</button>
-</form>
-    
+            </p>
+
+
+            <button type="submit" name="Send" class="registerbtn">Submit</button>
+    </form>
+
 </body>
+
 </html>
